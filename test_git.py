0069cffa298ced167b6e3d07225de4e65b8716c1
SuperSaiyan <809605798@qq.com>
2019-04-05 21:04:03 +0800

    在IDE测试commit: test_git

---
+++ test_git.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# author:DWB
+# datetime:2019/4/5 20:52
+# IDE: PyCharm
+# Email: dwb5868@gmail.com
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
